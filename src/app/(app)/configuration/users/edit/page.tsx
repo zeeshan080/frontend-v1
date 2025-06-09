86c5d29741e8ee16e5e967d686a4d214a73a1c7b
Zeeshan Aziz <80561224+zeeshan080@users.noreply.github.com>
2025-06-09 15:27:16 +0500

    refactor: clean up formatting and improve readability in EditPage component feat: added server side search param id fetch

--- src/app/(app)/configuration/users/edit/page.tsx
+++ src/app/(app)/configuration/users/edit/page.tsx
@@ -1,22 +1,34 @@
-import UserFom from '@/components/configuration/user-form';
-import * as React from 'react';
+import UserFom from "@/components/configuration/user-form";
+import * as React from "react";
 
 export interface IEditPageProps {
+  searchParams: Promise<{ [key: string]: string | string[] }>;
 }
-const data = {
-        id: "b3b6e7e2-1c1a-4e3a-9c2a-1e2b3c4d5e6f",
-        username: "alice",
-        email: "alice@example.com",
-        password: "password123",
-        role: "admin",
-        is_active: true,
-        is_superuser: true,
-    }
 
-export default function EditPage (props: IEditPageProps) {
+const data = {
+  id: "b3b6e7e2-1c1a-4e3a-9c2a-1e2b3c4d5e6f",
+  username: "alice",
+  email: "alice@example.com",
+  password: "password123",
+  role: "admin",
+  is_active: true,
+  is_superuser: true,
+};
+
+export default async function EditPage(props: IEditPageProps) {
+  const searchParams = await props.searchParams
+  const id = searchParams.id;
+
+  // Fetch user data by id
+  //const data = id ? await getUserById(id) : null;
+
+  console.log("id edit-->", id);
+
+  // Optionally, you can use the id to fetch or update data here
+
   return (
     <section>
-        <UserFom defaultValues={data}/>
+      <UserFom defaultValues={data} />
     </section>
   );
 }
